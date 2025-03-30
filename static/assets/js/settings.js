fd945f4288a2a89cc6f129c224187d901dd07942
isburnett <ian.sol.burnett@gmail.com>
2025-03-30 07:15:26 -0400

    Update settings.js

--- static/assets/js/settings.js
+++ static/assets/js/settings.js
@@ -429,7 +429,7 @@ function importSaveData() {
           });
         }
         alert('Your save data has been imported. Please test it out.')
-        alert('If you find any issues then report it in GitHub or the Interstellar Discord.')
+        alert('If you find any issues then report it in GitHub.')
       } catch (error) {
         console.error('Error parsing JSON file:', error);
       }
